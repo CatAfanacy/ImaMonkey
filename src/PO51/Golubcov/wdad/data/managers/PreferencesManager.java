e02a4651b6bda54581abc4fa394cd1f77156adad
CatAfanacy <catafanacy@gmail.com>
2017-10-04 22:59:23 +0400

    Task 6. Complete
    Almost finish

--- src/PO51/Golubcov/wdad/data/managers/PreferencesManager.java
+++ src/PO51/Golubcov/wdad/data/managers/PreferencesManager.java
@@ -9,6 +9,7 @@ import java.io.File;
 public class PreferencesManager {
 
     private static volatile PreferencesManager instance;
+
     private final Document doc;
     private final String pathConfig = "src\\PO51\\Golubcov\\wdad\\resources\\configuration\\appconfig.xml";
 
@@ -23,63 +24,57 @@ public class PreferencesManager {
     }
 
     private PreferencesManager() throws Exception {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-        DocumentBuilder db = dbf.newDocumentBuilder();
-        doc = db.parse(new File(pathConfig));
-    }
-
-    createregistry
-    registryaddress
-    registryport
-
- policypath
- usecodebaseonly
-    classprovider
-
-
-    public String getCreaterRegistry() {
-        String registry;
-                
-        return registry;
+        DocumentBuilderFactory docBFac = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docB = docBFac.newDocumentBuilder();
+        doc = docB.parse(new File(pathConfig));
     }
 
     public String getCreaterRegistry() {
-        return registry;
-    }
-    public String getCreaterRegistry() {
-        return registry;
-    }
-    public String getCreaterRegistry() {
-        return registry;
-    }
-    public String getCreaterRegistry() {
-        return registry;
-    }
-    public String getCreaterRegistry() {
+        String registry = doc.getElementsByTagName("createregistry").item(0).getTextContent();
         return registry;
     }
 
-
-    public String setCreaterRegistry() {
-        return registry;
+    public String getRegistryAdress() {
+        String regAd = doc.getElementsByTagName("registryaddress").item(0).getTextContent();
+        return regAd;
     }
 
-    public String setCreaterRegistry() {
-        return registry;
+    public String getRegistryPort() {
+        String regPort = doc.getElementsByTagName("registryport").item(0).getTextContent();
+        return regPort;
     }
-    public String setCreaterRegistry() {
-        return registry;
+    public String getPolicyPath() {
+        String polPath = doc.getElementsByTagName("policypath").item(0).getTextContent();
+        return polPath;
     }
-    public String setCreaterRegistry() {
-        return registry;
+    public String getUseCodeBaseOnly() {
+        String codeBase = doc.getElementsByTagName("usecodebaseonly").item(0).getTextContent();
+        return codeBase;
     }
-    public String setCreaterRegistry() {
-        return registry;
-    }
-    public String setCreaterRegistry() {
-        return registry;
+    public String getClassProvider() {
+        String cP = doc.getElementsByTagName("classprovider").item(0).getTextContent();
+        return cP;
     }
 
+    public void setCreaterRegistry(String newRegistry) {
+        doc.getElementsByTagName("createregistry").item(0).setTextContent(newRegistry);
+    }
 
+    public void getRegistryAdress(String newRegAd) {
+        doc.getElementsByTagName("registryaddress").item(0).setTextContent(newRegAd);
+    }
+
+    public void getRegistryPort(String newRegPort) {
+        doc.getElementsByTagName("registryport").item(0).setTextContent(newRegPort);
+    }
+    public void setPolicyPath(String newPolPath) {
+        doc.getElementsByTagName("policypath").item(0).setTextContent(newPolPath);
+    }
+    public void setUseCodeBaseOnly(String newCodeBase ) {
+        doc.getElementsByTagName("usecodebaseonly").item(0).setTextContent(newCodeBase);
+    }
+    public void setClassProvider(String newCP) {
+         doc.getElementsByTagName("classprovider").item(0).setTextContent(newCP);
+    }
 
 }
